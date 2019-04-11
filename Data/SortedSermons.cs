14ae603d3c2b9721aec322450bfc118fa6c65d5b
willomae <wilburomae@gmail.com>
2019-04-11 13:40:44 +0300

    created a class to be used while sorting sermons, containing a parent and children;

---
+++ Data/SortedSermons.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace MySermonsWPF.Data
+{
+    /// <summary>
+    /// Class used while sorting sermons. It defines the parent (based on the filter) and its children (that match the filter).
+    /// </summary>
+    public class SortedSermons
+    {
+        /// <summary>
+        /// The common value of a specified attribute of sermons based on the filter.
+        /// </summary>
+        public string Parent { get; set; }
+        /// <summary>
+        /// A list of sermons that share a parent based on the filter.
+        /// </summary>
+        public List<Sermon> Children { get; set; }
+    }
+}
