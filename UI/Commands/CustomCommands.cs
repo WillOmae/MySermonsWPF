e6bb11dfcff58e3f0065048c2e2face98c415ba2
willomae <wilburomae@gmail.com>
2019-04-14 13:27:56 +0300

    created class for custom commands;

---
+++ UI/Commands/CustomCommands.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Input;
+
+namespace MySermonsWPF.UI.Commands
+{
+    public static class CustomCommands
+    {
+        public static readonly RoutedUICommand Exit = new RoutedUICommand(
+            text: "Exit",
+            name: "Exit",
+            ownerType: typeof(CustomCommands),
+            inputGestures: new InputGestureCollection() { new KeyGesture(Key.F4, ModifierKeys.Alt) }
+            );
+        public static readonly RoutedUICommand CloseAll = new RoutedUICommand(
+            text: "Close All",
+            name: "CloseAll",
+            ownerType: typeof(CustomCommands),
+            inputGestures: new InputGestureCollection() { new KeyGesture(Key.F4, ModifierKeys.Control) }
+            );
+    }
+}
