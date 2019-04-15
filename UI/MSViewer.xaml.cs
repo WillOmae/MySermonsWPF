772b20040fa5b9cd4bd623e681e7b004b7a587ad
willomae <wilburomae@gmail.com>
2019-04-15 16:21:50 +0300

    made certain fields private; added public properties to abstract internal controls;

--- UI/MSViewer.xaml.cs
+++ UI/MSViewer.xaml.cs
@@ -5,11 +5,47 @@ namespace MySermonsWPF.UI
     /// <summary>
     /// Interaction logic for Viewer.xaml
     /// </summary>
-    public partial class Viewer:UserControl
+    public partial class MSViewer:UserControl
     {
-        public Viewer()
+        /// <summary>
+        /// Rich text document manager.
+        /// </summary>
+        private MSDocumentManager documentManager;
+        /// <summary>
+        /// Viewer title string.
+        /// </summary>
+        public string ViewerTitle
+        {
+            get
+            {
+                return this.MSViewerTitle.Text;
+            }
+            set
+            {
+                this.MSViewerTitle.Text = value;
+            }
+        }
+        /// <summary>
+        /// Rich text content string.
+        /// </summary>
+        public string ViewerContent
+        {
+            get
+            {
+                return this.documentManager.GetRichText();
+            }
+            set
+            {
+                this.documentManager.SetRichText(value);
+            }
+        }
+        /// <summary>
+        /// Viewer constructor.
+        /// </summary>
+        public MSViewer()
         {
             this.InitializeComponent();
+            this.documentManager = new MSDocumentManager(this.MSViewerContent);
         }
     }
 }
