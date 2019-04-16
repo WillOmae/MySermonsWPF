2ba315343f5ee6971b83f3e9e820e7a93d0ae254
willomae <wilburomae@gmail.com>
2019-04-16 04:18:37 +0300

    created a class to handle printing;

---
+++ UI/MSPrinter.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using MySermonsWPF.Data;
+
+namespace MySermonsWPF.UI
+{
+    public class MSPrinter
+    {
+        private Sermon sermon;
+        public MSPrinter(Sermon sermon)
+        {
+            this.sermon = sermon;
+        }
+        public void Print()
+        {
+            if(this.sermon != null && !string.IsNullOrEmpty(this.sermon.Content))
+            {
+                FlowDocument flowDocument = new FlowDocument();
+                using(Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(this.sermon.Content)))
+                {
+                    TextRange textRange = new TextRange(flowDocument.ContentStart, flowDocument.ContentEnd);
+                    textRange.Load(stream, DataFormats.Rtf);
+                }
+                PrintDialog printDialog = new PrintDialog();
+                if(printDialog.ShowDialog() == true)
+                {
+                    printDialog.PrintDocument(((IDocumentPaginatorSource)(flowDocument)).DocumentPaginator, "Trial");
+                }
+            }
+        }
+    }
+}
