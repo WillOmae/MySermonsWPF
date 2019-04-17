8d54fee0c2f52859850eb39434f9233c54d2eafa
willomae <wilburomae@gmail.com>
2019-04-18 01:18:45 +0300

    introduced commands and handled some events;

--- UI/MSRichTextBox.xaml.cs
+++ UI/MSRichTextBox.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 using MySermonsWPF.Data;
 
 namespace MySermonsWPF.UI
@@ -8,11 +11,56 @@ namespace MySermonsWPF.UI
     /// </summary>
     public partial class MSRichTextBox:UserControl
     {
+        private SolidColorBrush white = new SolidColorBrush(Color.FromRgb(0xFF, 0xFF, 0xFF));
+        private SolidColorBrush grey = new SolidColorBrush(Color.FromArgb(0xFF, 0xDD, 0xDD, 0xDD));
         private Sermon sermon;
         public MSRichTextBox(Sermon sermon)
         {
             this.InitializeComponent();
             this.sermon = sermon;
+        }
+
+        private void MSRichTextBoxLoaded(object sender, RoutedEventArgs e)
+        {
+            this.SetButtonsProperties();
+            MessageBox.Show(this.RTBPastePlain.Background.ToString());
+        }
+        /// <summary>
+        /// Set the properties of buttons in bulk.
+        /// </summary>
+        private void SetButtonsProperties()
+        {
+            // first, get the buttons.
+            foreach(var button in MSFindVisualChildren.FindVisualChildren<Button>(this.BaseFormattingBar))
+            {
+                button.IsEnabledChanged += this.Button_IsEnabledChanged;
+                button.Click += delegate
+                {
+                    this.BaseRichTextBox.Focus();
+                };
+            }
+        }
+
+        private void Button_IsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            ((Button)sender).Background = (bool)e.NewValue ? this.white : this.grey;
+        }
+
+        private void FindCommandExecuted(object target, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("finding...");
+        }
+        private void FindCommandCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+        private void PastePlainCommandExecuted(object target, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("pasting plain...");
+        }
+        private void PastePlainCommandCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
         }
     }
 }
