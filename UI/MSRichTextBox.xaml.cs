34d9f9ccb3b08654e1781b2697390e166999ebd8
willomae <wilburomae@gmail.com>
2019-04-19 03:15:09 +0300

    save function discriminates between existing and new sermons;

--- UI/MSRichTextBox.xaml.cs
+++ UI/MSRichTextBox.xaml.cs
@@ -54,7 +54,7 @@ namespace MySermonsWPF.UI
 
         private void SaveCommandExecuted(object target, ExecutedRoutedEventArgs e)
         {
-            MessageBox.Show("saving...");
+            this.Save();
         }
 
         private void SaveCommandCanExecute(object sender, CanExecuteRoutedEventArgs e)
@@ -121,10 +121,17 @@ namespace MySermonsWPF.UI
             var keyText = "Gal 3:24";
             if(this.sermon == null)
             {
+                // the sermon does not exist; create
                 this.sermon = new Sermon(title, location, themes, keyText, string.Empty, content);
+                MessageBox.Show("Creation success: " + this.sermon.Create());
             }
             else
             {
+                // the sermon exists; update
+                var id = this.sermon.ID;
+                var guid = this.sermon.GUID;
+                this.sermon = new Sermon(id, guid, title, location.ID, location.Name, location, themes, this.sermon.DateCreated.Ticks, this.sermon.DateLastAccessed.Ticks, keyText, this.sermon.OtherMetaData, content);
+                MessageBox.Show("Update successful: " + this.sermon.Update());
             }
         }
     }
