8831e4d3d1e0845636335a0b1f61b42392d55241
willomae <wilburomae@gmail.com>
2019-04-24 00:55:43 +0300

    open treeview item on double click;

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using MySermonsWPF.Data;
@@ -12,11 +13,13 @@ namespace MySermonsWPF
     public partial class MainWindow:Window
     {
         public static List<SortedSermons> Sermons { get; set; }
+        private static List<Sermon> _sermons { get; set; }
         public MainWindow()
         {
             if(Database.Initialise())
             {
-                Sermons = Sermon.Sort(SermonFilters.Location, Sermon.Read());
+                _sermons = Sermon.Read();
+                Sermons = Sermon.Sort(SermonFilters.Location, _sermons);
 
                 this.InitializeComponent();
                 this.MSTabControl.Items.Add(new TabItem()
@@ -28,6 +31,28 @@ namespace MySermonsWPF
             else
             {
                 MessageBox.Show("Database initialisation failed.");
+            }
+        }
+
+        private void TreeViewEntry_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if(e.ClickCount == 2 && (sender is TextBlock textBlock))
+            {
+                string guid = textBlock.Tag.ToString();
+                var sermon = (from x in Sermons
+                              from y in x.Children
+                              where y.GUID.Equals(guid)
+                              select y).FirstOrDefault();
+                if(sermon != null)
+                {
+                    TabItem tabItem = new TabItem()
+                    {
+                        Header = sermon.Title,
+                        Content = new MSViewer(sermon)
+                    };
+                    this.MSTabControl.Items.Add(tabItem);
+                    this.MSTabControl.SetSelectedItem(tabItem);
+                }
             }
         }
     }
