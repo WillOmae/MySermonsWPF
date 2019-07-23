e50908b31b115b39c01211d4070d491c357a0eb4
willomae <wilburomae@gmail.com>
2019-07-23 22:12:40 +0300

    Added a class defining a Bible chapter.

---
+++ Data/Bible/BibleChapter.cs
@@ -0,0 +1,27 @@
+﻿namespace MySermonsWPF.Data.Bible
+{
+    /// <summary>
+    /// Class defining a Bible Chapter.
+    /// </summary>
+    public class BibleChapter
+    {
+        /// <summary>
+        /// Chapter identifier.
+        /// </summary>
+        public int ChapterId { get; }
+        /// <summary>
+        /// Number of verses in the chapter.
+        /// </summary>
+        public int VerseCount { get; }
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        /// <param name="chapter">Chapter identifier.</param>
+        /// <param name="verseCount">Number of verses in chapter.</param>
+        public BibleChapter(int chapter, int verseCount)
+        {
+            ChapterId = chapter;
+            VerseCount = verseCount;
+        }
+    }
+}
