d275efb212272e9f97190a7bbf3380788c58b83f
willomae <wilburomae@gmail.com>
2019-07-24 08:52:22 +0300

    Added a class for Bible verses.

---
+++ Data/Bible/BibleVerse.cs
@@ -0,0 +1,41 @@
+﻿namespace MySermonsWPF.Data.Bible
+{
+    /// <summary>
+    /// Class defining a Bible verse.
+    /// </summary>
+    public class BibleVerse
+    {
+        /// <summary>
+        /// Shorthand book-chapter-verse reference.
+        /// </summary>
+        public string BCV { get; }
+        /// <summary>
+        /// Human-readable verse reference.
+        /// </summary>
+        public string Reference { get; }
+        /// <summary>
+        /// Verse content.
+        /// </summary>
+        public string Content { get; }
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        /// <param name="bcv">Shorthand book-chapter-verse reference.</param>
+        /// <param name="reference">Human-readable verse reference.</param>
+        /// <param name="content">Verse content.</param>
+        public BibleVerse(string bcv, string reference, string content)
+        {
+            BCV = bcv;
+            Reference = reference;
+            Content = content;
+        }
+        /// <summary>
+        /// Return a string representation of the object.
+        /// </summary>
+        /// <returns>The string representation of the object.</returns>
+        public override string ToString()
+        {
+            return Reference;
+        }
+    }
+}
