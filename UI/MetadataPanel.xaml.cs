8d7a8006d1d1b79f595718323216fea19c648273
willomae <wilburomae@gmail.com>
2019-10-20 15:51:30 +0300

    moved metadata panel to its own class

---
+++ UI/MetadataPanel.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows.Controls;
+
+namespace MySermonsWPF.UI
+{
+    /// <summary>
+    /// Interaction logic for MetadataPanel.xaml
+    /// </summary>
+    public partial class MetadataPanel : UserControl
+    {
+        public MetadataPanel()
+        {
+            InitializeComponent();
+        }
+        public void Populate(string title, string speaker, string keytext, string location, string themes, string otherinfo)
+        {
+            this.MetaTitle.Text = title ?? string.Empty;
+            this.MetaSpeaker.Text = speaker ?? string.Empty;
+            this.MetaKeyText.Text = keytext ?? string.Empty;
+            this.MetaLocation.Text = location ?? string.Empty;
+            this.MetaThemes.Text = themes ?? string.Empty;
+            this.MetaOtherInfo.Text = otherinfo ?? string.Empty;
+        }
+        public (string title, string speaker, string keytext, string location, string themes, string otherinfo) GetMetadata()
+        {
+            return (this.MetaTitle.Text,
+                    this.MetaSpeaker.Text,
+                    this.MetaKeyText.Text,
+                    this.MetaLocation.Text,
+                    this.MetaThemes.Text,
+                    this.MetaOtherInfo.Text);
+        }
+        public bool Verify()
+        {
+            if (string.IsNullOrEmpty(this.MetaTitle.Text))
+            {
+                this.MetaTitle.Focus();
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
