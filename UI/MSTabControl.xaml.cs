0b077c56b21257bd48adf3caefb67c9e70008d34
willomae <wilburomae@gmail.com>
2019-11-18 22:18:42 +0300

    some optimisations

--- UI/MSTabControl.xaml.cs
+++ UI/MSTabControl.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.Linq;
-using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
@@ -10,7 +9,7 @@ namespace MySermonsWPF.UI
     /// <summary>
     /// Interaction logic for MSTabControl.xaml
     /// </summary>
-    public partial class MSTabControl:UserControl
+    public partial class MSTabControl : UserControl
     {
         public ObservableCollection<TabItem> Items = new ObservableCollection<TabItem>();
 
@@ -18,19 +17,18 @@ namespace MySermonsWPF.UI
         {
             this.InitializeComponent();
             this.Items.CollectionChanged += this.Items_CollectionChanged;
-
         }
 
         private void Items_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
         {
-            switch(e.Action)
+            switch (e.Action)
             {
                 case NotifyCollectionChangedAction.Add:
-                    foreach(var item in e.NewItems)
+                    foreach (var item in e.NewItems)
                     {
-                        if(item is TabItem tabItem)
+                        if (item is TabItem tabItem)
                         {
-                            if(tabItem.Header is string headerText)
+                            if (tabItem.Header is string headerText)
                             {
                                 MSTabItemHeader tabItemHeader = new MSTabItemHeader();
                                 tabItemHeader.BaseTabItemHeaderText.Content = headerText;
@@ -48,50 +46,53 @@ namespace MySermonsWPF.UI
                                 };
                             }
                             // prevent duplicate viewers
-                            if(tabItem.Content is MSViewer viewer)
+                            if (tabItem.Content is MSViewer viewer)
                             {
                                 var sermon = viewer.GetSermon();
                                 ItemCollection collection = this.BaseTabControl.Items;
-                                var found = (from TabItem collectionItem in collection
-                                             where (collectionItem.Content as MSViewer) != null && (collectionItem.Content as MSViewer).GetSermon().Equals(sermon)
-                                             select collectionItem).FirstOrDefault();
-                                if(found != null)
+                                TabItem found = (from TabItem collectionItem in collection
+                                                 where (collectionItem.Content as MSViewer) != null && (collectionItem.Content as MSViewer).GetSermon().Equals(sermon)
+                                                 select collectionItem).FirstOrDefault();
+                                if (found == null)
                                 {
-                                    this.Items.Remove(tabItem);
-                                    BaseTabControl.SelectedItem = found;
+                                    this.BaseTabControl.Items.Add(tabItem);
+                                    BaseTabControl.SelectedItem = tabItem;
                                 }
                                 else
                                 {
-                                    this.BaseTabControl.Items.Add(tabItem);
+                                    this.Items.Remove(tabItem);
+                                    BaseTabControl.SelectedItem = found;
                                 }
                             }
                             // prevent duplicate editors
                             else if (tabItem.Content is MSRichTextBoxAdv richTextBox)
                             {
                                 var sermon = richTextBox.GetSermon();
-                                ItemCollection collection = this.BaseTabControl.Items;
-                                var found = (from TabItem collectionItem in collection
-                                             where (collectionItem.Content as MSRichTextBoxAdv) != null && (collectionItem.Content as MSRichTextBoxAdv).GetSermon() != null && (collectionItem.Content as MSRichTextBoxAdv).GetSermon().Equals(sermon)
-                                             select collectionItem).FirstOrDefault();
-                                if (found != null)
+                                var collection = this.BaseTabControl.Items.OfType<TabItem>();
+                                TabItem found = collection.Where(t => t.Content is MSRichTextBoxAdv mSRichTextBoxAdv
+                                                                  && mSRichTextBoxAdv.GetSermon() != null
+                                                                  && mSRichTextBoxAdv.GetSermon().Equals(sermon)).FirstOrDefault();
+                                if (found == null)
                                 {
-                                    this.Items.Remove(tabItem);
-                                    BaseTabControl.SelectedItem = found;
+                                    this.BaseTabControl.Items.Add(tabItem);
+                                    BaseTabControl.SelectedItem = tabItem;
                                 }
                                 else
                                 {
-                                    this.BaseTabControl.Items.Add(tabItem);
+                                    this.Items.Remove(tabItem);
+                                    BaseTabControl.SelectedItem = found;
                                 }
                             }
                             else
                             {
                                 this.BaseTabControl.Items.Add(tabItem);
+                                BaseTabControl.SelectedItem = tabItem;
                             }
                         }
                     }
                     break;
                 case NotifyCollectionChangedAction.Remove:
-                    foreach(var item in e.OldItems)
+                    foreach (var item in e.OldItems)
                     {
                         this.BaseTabControl.Items.Remove(item);
                     }
@@ -99,27 +100,16 @@ namespace MySermonsWPF.UI
             }
         }
 
-        private void TabItemHeader_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            MessageBox.Show("Clicked");
-        }
-
-        private void SetDoubleClickToClose(TabItem tabItem)
-        {
-            if(tabItem == null) return;
-            tabItem.MouseDoubleClick += (s, e) => this.Items.Remove(tabItem);
-        }
-
         private void CloseCommandExecuted(object target, ExecutedRoutedEventArgs e)
         {
-            if(this.BaseTabControl.SelectedItem is TabItem tabItem)
+            if (this.BaseTabControl.SelectedItem is TabItem tabItem)
             {
                 this.Items.Remove(tabItem);
             }
         }
         private void CloseCommandCanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
-            e.CanExecute = this.Items.Count > 0 ? true : false;
+            e.CanExecute = this.Items.Any();
         }
         private void CloseAllCommandExecuted(object target, ExecutedRoutedEventArgs e)
         {
@@ -128,7 +118,7 @@ namespace MySermonsWPF.UI
         }
         private void CloseAllCommandCanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
-            e.CanExecute = this.Items.Count > 0 ? true : false;
+            e.CanExecute = this.Items.Any();
         }
         public void SetSelectedItem(TabItem tabItem)
         {
