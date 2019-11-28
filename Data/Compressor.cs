bb4e5fedc2b26ecf94df0ccc619d5132c76b69f8
willomae <wilburomae@gmail.com>
2019-11-29 01:49:03 +0300

    added GZip string compressor/decompressor

---
+++ Data/Compressor.cs
@@ -0,0 +1,45 @@
+﻿using System.IO.Compression;
+using System.IO;
+
+namespace MySermonsWPF.Data
+{
+    public static class Compressor
+    {
+        /// <summary>
+        /// Compress a byte array. Null checking enabled.
+        /// </summary>
+        /// <param name="input">Input bytes</param>
+        /// <returns></returns>
+        public static byte[] Compress(byte[] input)
+        {
+            using (MemoryStream outputStream = new MemoryStream())
+            {
+                using (GZipStream zipStream = new GZipStream(outputStream, CompressionMode.Compress))
+                {
+                    zipStream.Write(input, 0, input.Length);
+                    zipStream.Close();
+                    return outputStream.ToArray();
+                }
+            }
+        }
+        /// <summary>
+        /// Decompress a byte array. Null checking enabled.
+        /// </summary>
+        /// <param name="input">Input bytes</param>
+        /// <returns></returns>
+        public static byte[] Decompress(byte[] input)
+        {
+            using (MemoryStream inputStream = new MemoryStream(input))
+            {
+                using (GZipStream zipStream = new GZipStream(inputStream, CompressionMode.Decompress))
+                {
+                    using (MemoryStream outputStream = new MemoryStream())
+                    {
+                        zipStream.CopyTo(outputStream);
+                        return outputStream.ToArray();
+                    }
+                }
+            }
+        }
+    }
+}
